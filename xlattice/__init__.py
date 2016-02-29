f86d16b6e21e626fb129922496946d7c1621c8b3
Jim Dixon <jddixon@gmail.com>
2016-02-29 08:47:51 -0800

    update LICENSE

--- xlattice/__init__.py
+++ xlattice/__init__.py
@@ -8,8 +8,8 @@ __all__ = [ '__version__',      '__version_date__',
             'SHA2_HEX_NONE',
         ]
 
-__version__      = '0.3.2'
-__version_date__ = '2016-02-02'
+__version__      = '0.3.3'
+__version_date__ = '2016-02-29'
 
 
 # This is the SHA1 of an empty string (or file)
@@ -33,6 +33,7 @@ SHA2_HEX_LEN    = 64
 # Binary values 
 SHA1_BIN_NONE = binascii.a2b_hex(SHA1_HEX_NONE)
 SHA2_BIN_NONE = binascii.a2b_hex(SHA2_HEX_NONE)
+
 
 
 
