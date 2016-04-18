d9fd7f0890406500480f65dbfa9ce02ffdc4167a
Jim Dixon <jddixon@gmail.com>
2016-04-18 10:23:12 -0700

    add uPreen, improve uConsolidate

--- xlattice/stats.py
+++ xlattice/stats.py
@@ -2,7 +2,11 @@
 
 # ~/dev/py/xlattice_py/stats
 
-import os, re, shutil, stat, sys
+import os
+import re
+import shutil
+import stat
+import sys
 from argparse import ArgumentParser
 
 from xlattice import (__version__, __version_date__)
@@ -13,27 +17,29 @@ from xlattice.u import fileSHA1Hex
 ###############################
 
 HEX2_PAT = '^[0-9a-f][0-9a-f]$'
-HEX2_RE  = re.compile(HEX2_PAT)
+HEX2_RE = re.compile(HEX2_PAT)
+
 
 def scanLeafDir(pathToDir):
     fileCount = 0
-    oddCount  = 0
+    oddCount = 0
     for file in os.listdir(pathToDir):
         m = HEX2_RE.match(file)
         if m:
             fileCount = fileCount + 1
         else:
-            occCount  = oddCount + 1
+            occCount = oddCount + 1
     return fileCount, oddCount
+
 
 def collectStats(inDir, outDir, verbose):
 
     leafCount = 0
     subDirCount = 0
     subSubDirCount = 0
-    oddCount       = 0
-    hasL            = False
-    hasNodeID       = False
+    oddCount = 0
+    hasL = False
+    hasNodeID = False
 
     if outDir:
         os.makedirs(outDir, exist_ok=True)
@@ -41,14 +47,14 @@ def collectStats(inDir, outDir, verbose):
     # top level files / subdirectories
     topFiles = os.listdir(inDir)
     for topFile in topFiles:
-       
+
         # -- top level files ----------------------------------------
 
-        # At this level we expect 00-ff, tmp/ and in/ subdirectories 
+        # At this level we expect 00-ff, tmp/ and in/ subdirectories
         # plus the files L and possibly nodeID.
-        
+
         m = HEX2_RE.match(topFile)
-        if m: 
+        if m:
 
             # -- mid-level directories ------------------------------
 
@@ -64,8 +70,8 @@ def collectStats(inDir, outDir, verbose):
                         subSubDirCount = subSubDirCount + 1
                         n, odd = scanLeafDir(pathToSubSubDir)
                         leafCount = leafCount + n
-                        oddCount  = oddCount + odd
-                        
+                        oddCount = oddCount + odd
+
                 # -- other mid-level files --------------------------
                 else:
                     pathToOddity = os.path.join(pathToSubDir, midFile)
@@ -83,10 +89,10 @@ def collectStats(inDir, outDir, verbose):
                 pathToDir = os.path.join(inDir, topFile)
                 n, odd = scanLeafDir(pathToDir)
                 leafCount = leafCount + n
-                oddCount  = oddCount  + odd
+                oddCount = oddCount + odd
             else:
                 pathToOddity = os.path.join(inDir, topFile)
                 print("unexpected: %s" % pathToOddity)
                 oddCount = oddCount + 1
-    
+
     return subDirCount, subSubDirCount, leafCount, oddCount, hasL, hasNodeID
