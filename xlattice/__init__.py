6d34ce07078b851b352563f92b5e69126e963c63
Jim Dixon <jddixon@gmail.com>
2016-05-11 12:37:51 -0700

    missed files in last commit?

--- xlattice/__init__.py
+++ xlattice/__init__.py
@@ -8,8 +8,8 @@ __all__ = ['__version__', '__version_date__',
            'SHA2_HEX_NONE',
            ]
 
-__version__ = '0.6.0'
-__version_date__ = '2016-05-10'
+__version__ = '0.6.1'
+__version_date__ = '2016-05-11'
 
 
 # This is the SHA1 of an empty string (or file)
