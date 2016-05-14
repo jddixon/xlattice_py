40038aa6c94a19d9f1d54d14a5263e582845b7a8
Jim Dixon <jddixon@gmail.com>
2016-05-14 16:53:33 -0700

    introduce xlattice/u/u{Flat,16,256}.py

--- testU16SHA1.py
+++ testU16SHA1.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 
-# xlattice_py/testSHA2.py
+# xlattice_py/testU16SHA1.py
 
 import binascii
+import hashlib
 import os
 import re
 import time
 import unittest
-import hashlib
-import sys
-from xlattice import u
+from xlattice.u import u16
 from rnglib import SimpleRNG
 
-DATA_PATH = 'myData'
-U_PATH = 'myU2'
-U_TMP_PATH = 'myU2/tmp'
-
-#######################################################
-# OBSOLESCENT: THIS FILE IS REPLACED BY testU256SHA2.py
-#######################################################
+DATA_PATH = 'myData'      # contains files of random data
+U_PATH = 'myU1'        # those same files stored by content hash
+U_TMP_PATH = 'myU1/tmp'
 
 
-class TestSHA2 (unittest.TestCase):
+class TestSHA1 (unittest.TestCase):
 
     def setUp(self):
         self.rng = SimpleRNG(time.time())
@@ -39,110 +34,110 @@ class TestSHA2 (unittest.TestCase):
     # utility functions #############################################
 
     # actual unit tests #############################################
-    def testCopyAndPut(self):
-        """we are testing sha1Key = u.copyAndPut2(path, uPath, key) """
+    def testCopyAndPut1(self):
+        """we are testing sha1Key = u16.copyAndPut1(path, uPath, key) """
 
         # create a random file                          maxLen   minLen
         (dLen, dPath) = self.rng.nextDataFile(DATA_PATH, 16 * 1024, 1)
-        dKey = u.fileSHA2Hex(dPath)
+        dKey = u16.fileSHA1Hex(dPath)
 
         # invoke function
-        (uLen, uKey) = u.copyAndPut2(dPath, U_PATH, dKey)
+        (uLen, uKey) = u16.copyAndPut1(dPath, U_PATH, dKey)
         self.assertEqual(dLen, uLen)
         self.assertEqual(dKey, uKey)
 
         # verify that original and copy both exist
         self.assertTrue(os.path.exists(dPath))
-        uPath = u.getPathForKey(U_PATH, uKey)
+        uPath = u16.getPathForKey(U_PATH, uKey)
         self.assertTrue(os.path.exists(uPath))
 
-        dKeyBin = u.fileSHA2Bin(dPath)
+        dKeyBin = u16.fileSHA1Bin(dPath)
         dKeyHex = binascii.b2a_hex(dKeyBin).decode('utf-8')
         self.assertEqual(dKeyHex, dKey)
 
     def testExists(self):
-        """we are testing whether = u.exists(uPath, key) """
+        """we are testing whether = u16.exists(uPath, key) """
 
         (dLen, dPath) = self.rng.nextDataFile(DATA_PATH, 16 * 1024, 1)
-        dKey = u.fileSHA2Hex(dPath)
-        (uLen, uKey) = u.copyAndPut2(dPath, U_PATH, dKey)
-        uPath = u.getPathForKey(U_PATH, uKey)
+        dKey = u16.fileSHA1Hex(dPath)
+        (uLen, uKey) = u16.copyAndPut1(dPath, U_PATH, dKey)
+        uPath = u16.getPathForKey(U_PATH, uKey)
         self.assertTrue(os.path.exists(uPath))
-        self.assertTrue(u.exists(U_PATH, uKey))
+        self.assertTrue(u16.exists(U_PATH, uKey))
         os.unlink(uPath)
         self.assertFalse(os.path.exists(uPath))
-        self.assertFalse(u.exists(U_PATH, uKey))
+        self.assertFalse(u16.exists(U_PATH, uKey))
 
     def testFileLen(self):
-        """we are testing len = u.fileLen(uPath, key) """
+        """we are testing len = u16.fileLen(uPath, key) """
 
         (dLen, dPath) = self.rng.nextDataFile(DATA_PATH, 16 * 1024, 1)
-        dKey = u.fileSHA2Hex(dPath)
-        (uLen, uKey) = u.copyAndPut2(dPath, U_PATH, dKey)
-        uPath = u.getPathForKey(U_PATH, uKey)
+        dKey = u16.fileSHA1Hex(dPath)
+        (uLen, uKey) = u16.copyAndPut1(dPath, U_PATH, dKey)
+        uPath = u16.getPathForKey(U_PATH, uKey)
         self.assertEqual(dLen, uLen)
-        self.assertEqual(dLen, u.fileLen(U_PATH, uKey))
+        self.assertEqual(dLen, u16.fileLen(U_PATH, uKey))
 
-    def testFileSHA2(self):
-        """ we are testing sha1Key = fileSHA2Hex(path) """
+    def testFileSHA1(self):
+        """ we are testing sha1Key = fileSHA1Hex(path) """
         (dLen, dPath) = self.rng.nextDataFile(DATA_PATH, 16 * 1024, 1)
         with open(dPath, 'rb') as f:
             data = f.read()
-        digest = hashlib.sha256()
+        digest = hashlib.sha1()
         digest.update(data)
         dKey = digest.hexdigest()
-        fsha1 = u.fileSHA2Hex(dPath)
+        fsha1 = u16.fileSHA1Hex(dPath)
         self.assertEqual(dKey, fsha1)
 
     def testGetPathForKey(self):
         """ we are testing path = getPathForKey(uPath, key) """
 
         (dLen, dPath) = self.rng.nextDataFile(DATA_PATH, 16 * 1024, 1)
-        dKey = u.fileSHA2Hex(dPath)
-        (uLen, uKey) = u.copyAndPut2(dPath, U_PATH, dKey)
-        uPath = u.getPathForKey(U_PATH, uKey)
+        dKey = u16.fileSHA1Hex(dPath)
+        (uLen, uKey) = u16.copyAndPut1(dPath, U_PATH, dKey)
+        uPath = u16.getPathForKey(U_PATH, uKey)
 
         # XXX implementation-dependent test
-        expectedPath = "%s/%s/%s/%s" % (U_PATH, uKey[0:2], uKey[2:4], uKey)
+        expectedPath = "%s/%s/%s/%s" % (U_PATH, uKey[0:1], uKey[1:2], uKey)
         self.assertEqual(expectedPath, uPath)
 
-    def testPut(self):
-        """we are testing (len,hash)  = put(inFile, uPath, key) """
+    def testPut1(self):
+        """we are testing (len,hash)  = put1(inFile, uPath, key) """
 
         (dLen, dPath) = self.rng.nextDataFile(DATA_PATH, 16 * 1024, 1)
-        dKey = u.fileSHA2Hex(dPath)
+        dKey = u16.fileSHA1Hex(dPath)
         with open(dPath, 'rb') as f:
             data = f.read()
         dupePath = os.path.join(DATA_PATH, dKey)
         with open(dupePath, 'wb') as f:
             f.write(data)
 
-        (uLen, uKey) = u.put2(dPath, U_PATH, dKey)
-        uPath = u.getPathForKey(U_PATH, uKey)
+        (uLen, uKey) = u16.put1(dPath, U_PATH, dKey)
+        uPath = u16.getPathForKey(U_PATH, uKey)
 
         # inFile is renamed
         self.assertFalse(os.path.exists(dPath))
-        self.assertTrue(u.exists(U_PATH, uKey))
+        self.assertTrue(u16.exists(U_PATH, uKey))
 
-        (dupeLen, dupeKey) = u.put2(dupePath, U_PATH, dKey)
+        (dupeLen, dupeKey) = u16.put1(dupePath, U_PATH, dKey)
         # dupe file is deleted'
         self.assertEqual(uKey, dupeKey)
         self.assertFalse(os.path.exists(dupePath))
-        self.assertTrue(u.exists(U_PATH, uKey))
+        self.assertTrue(u16.exists(U_PATH, uKey))
 
-    def testPutData(self):
-        """ we are testing (len,hash)  = putData2(data, uPath, key) """
+    def testPutData1(self):
+        """ we are testing (len,hash)  = putData1(data, uPath, key) """
 
         # this is just lazy coding ;-)
         (dLen, dPath) = self.rng.nextDataFile(DATA_PATH, 16 * 1024, 1)
-        dKey = u.fileSHA2Hex(dPath)
+        dKey = u16.fileSHA1Hex(dPath)
         with open(dPath, 'rb') as f:
             data = f.read()
 
-        (uLen, uKey) = u.putData2(data, U_PATH, dKey)
+        (uLen, uKey) = u16.putData1(data, U_PATH, dKey)
         self.assertEqual(dKey, uKey)
-        self.assertTrue(u.exists(U_PATH, dKey))
-        uPath = u.getPathForKey(U_PATH, uKey)
+        self.assertTrue(u16.exists(U_PATH, dKey))
+        uPath = u16.getPathForKey(U_PATH, uKey)
 
 
 if __name__ == '__main__':
--- xlattice/__init__.py
+++ xlattice/__init__.py
@@ -8,8 +8,8 @@ __all__ = ['__version__', '__version_date__',
            'SHA2_HEX_NONE',
            ]
 
-__version__ = '0.6.0'
-__version_date__ = '2016-05-10'
+__version__ = '1.0.0'
+__version_date__ = '2016-05-14'
 
 
 # This is the SHA1 of an empty string (or file)
