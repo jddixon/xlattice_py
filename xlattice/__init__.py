18c660f0058d1e37238cc6db70803c364b440795
Jim Dixon <jddixon@gmail.com>
2017-02-17 11:18:56 -0800

    add Conext, test_context.py

--- xlattice/__init__.py
+++ xlattice/__init__.py
@@ -38,10 +38,12 @@ __all__ = ['__version__', '__version_date__',
            'check_hashtype',
            'parse_hashtype_etc', 'fix_hashtype', 'show_hashtype_etc',
 
-           'check_u_path', ]
+           'check_u_path',
 
-__version__ = '1.7.5'
-__version_date__ = '2017-02-16'
+           'Context', 'ContextError', ]
+
+__version__ = '1.7.6'
+__version_date__ = '2017-02-17'
 
 
 # This is the SHA1 of an empty string (or file)
@@ -334,3 +336,124 @@ def showUsingSHA(args):
     return show_using_sha(args)
 #
 # END SYN -----------------------------------------------------------
+
+# ABSTRACTIONS ======================================================
+
+"""
+The XLattice Context.
+
+"""
+
+
+class ContextError(RuntimeError):
+    """ Handles Context-related exceptions. """
+
+# Any reason to make this ABCMeta ?
+
+
+class Context(dict):
+    """
+    The XLattice context.
+
+    A naming context consisting of a possibly nested set of name-to-object
+    bindings.  If there is a parent context and a key cannot be resolved
+    in this context, an attempt will be made to resolve it in the parent,
+    recursively.
+
+    Names added to the context must not be None.
+
+    This implementation is intended to be thread-safe.
+    """
+
+    def __init__(self, parent=None):
+        """ Create a Context, optionally with a parent. """
+        super().__init__()
+        self._parent = parent
+
+    def synchronize(self):
+        """
+        TBD magic.
+
+        This makes no sense as it is set out.
+        """
+
+    def bind(self, name, obj):        # -> Context
+        """
+        Bind a name to an Object at this Context level.  Neither name
+        nor object may be None.
+
+        If this context has a parent, the binding at this level will
+        mask any bindings in the parent and above.
+
+        @param name the name being bound
+        @param o    the Object it is bound to
+        @raises ContextError if either is None.
+        """
+
+        if name is None or obj is None:
+            raise ContextError("name or object is None")
+        self.synchronize()      # XXX needs to sync block
+        self[name] = obj
+        return self
+
+    def lookup(self, name):          # -> object
+        """
+        Looks up a name recursively.  If the name is bound at this level,
+        the object it is bound to is returned.  Otherwise, if there is
+        a parent Context, the value returned by a lookup in the parent
+        Context is returned.  If there is no parent and no match, returns
+        None.
+
+        @param name the name we are attempting to match
+        @return     the value the name is bound to at this or a higher level
+                    or None if there is no such value
+        """
+        if name is None:
+            raise ContextError("name cannot be None")
+        obj = None
+        self.synchronize()
+        if name in self:
+            obj = self[name]
+        elif self._parent is not None:
+            obj = self._parent.lookup(name)
+        return obj
+
+    def unbind(self, name):        # -> None
+        """
+        Remove a binding from the Context.  If there is no such binding,
+        silently ignore the request.  Any binding at a higher level, in
+        the parent Context or above, is unaffected by this operation.
+
+        @param name Name to be unbound.
+        """
+        self.synchronize()
+        # XXX Need to sync on block
+        if name is None:
+            raise ContextError("name is None")
+        # XXX will raise if not in dict
+        del self[name]
+
+    def size(self):                 # -> int
+        """ Return the number of bindings at this level. """
+        self.synchronize()          # XXX need to sync on block
+        return len(self)
+
+    @property
+    def parent(self):
+        """
+        Return a reference to the parent Context or None if there is none.
+        """
+        return self._parent
+
+    @parent.setter
+    def parent(self, new_parent):
+        """
+        Change the parent Context. This method returns a reference to
+        this instance, to allow method calls to be chained.
+
+        @param  new_parent New parent Context, possibly None.
+        Return a reference to the current Context
+        """
+
+        self._parent = new_parent
+        return self
