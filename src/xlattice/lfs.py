9c4010a6c77bbc3d508fbd23161e48062f3b048c
Jim Dixon <jddixon@gmail.com>
2017-09-10 18:20:22 -0700

    correct tox.ini, *requirements.txt

--- src/xlattice/lfs.py
+++ src/xlattice/lfs.py
@@ -1,17 +1,8 @@
 # xlattice_py/xlattice/lfs
 
-# import errno
 import os
-import warnings
 
-__all__ = ['mkdir_p', 'touch', ]
-
-
-def mkdir_p(path, mode=0o777):
-    # DROP ON REACHING v1.7 *****************************************
-    warnings.warn("mkdir_p deprecated", DeprecationWarning)
-    # Python 3
-    os.makedirs(path, mode, exist_ok=True)
+__all__ = ['touch', ]
 
 
 def touch(fname, times=None):
